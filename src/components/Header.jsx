262676e0b79c9f265f35c095e965f5b6f5b46892
João Pedro O. Braga <ojpbraga@gmail.com>
2024-08-17 23:04:39 -0300

    style: header I

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <header>
-        <div></div>
+    <header className='w-full flex justify-center top-3 fixed z-10'>
+        <div className='h-10 bg-slate-800 w-10/12 rounded-[14px] flex justify-between p-2'>
+          <p>JP</p>
+          <div>menu</div>
+        </div>
     </header>
   )
 }
