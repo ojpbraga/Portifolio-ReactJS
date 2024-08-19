202063bbc96a05fefca1bd1bfa914e79bd133c6a
João Pedro O. Braga <ojpbraga@gmail.com>
2024-08-19 00:05:52 -0300

    style: presentation add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import lights from "./images/lights.svg"
 import avatar from "./images/avatar.mp4"
+import tree from "./images/tree.svg"
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
         
         <section className="h-dvh flex flex-col items-center justify-between">
           <div className="flex flex-col items-center gap-5 w-5/6 md:w-[65vw] xl:flex-row xl:pt-[25vh] xl:w-[80vw] xl:justify-between xl:items-center">
-            {/* <img src="" alt="" /> */}
+            <img src={tree} alt="" className="hidden xl:block absolute z-[-1] h-[91vh] left-[-25px]" />
             {/* Text Presentation */}
             <div className="flex flex-col items-center pt-[12dvh] xl:w-[39.2vw] xl:pt-0">
               <h1 className="font-monoton leading-[75px] text-[11.71vw] md:text-7xl text-[#7799B0] md:leading-[95px]">Olá! Eu sou<br/><span className="text-[11.5dvh] max-[320px]:text-6xl md:text-9xl after:content-['Frontend'] after:text-[0.7rem] relative after:font-sans after:px-[5px] after:py-[2px] after:border-[#C0C0C0] after:border-2 after:rounded-md after:uppercase after:font-bold after:relative after:bottom-[50px] after:text-[#C0C0C0] text-[#00C6C6] after:md:left-4 after:md:bottom-16 after:md:text-sm">João</span></h1>
